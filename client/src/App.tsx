3f066e451881f6a14474ad2e41215a633ac55773
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-05-02 18:21:56 -0300

    carpeta client para unificar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import { FirstFilters } from "./components/firstFilters/FirstFilters";
 import { Landing } from "./components/landing/Landing";
 import { LogIn } from "./components/logIn/LogIn";
-import { SecondFilters } from "./components/secondFilters/SecondFilters";
 import { About } from "./components/about/About";
 import HomeContainer from "./components/homeContainer/HomeContainer";
 
@@ -12,16 +11,8 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Landing />} />
-<<<<<<< HEAD
         <Route path="/home" element={<HomeContainer />} />
-=======
-<<<<<<< HEAD
-=======
-        <Route path="/home" element={<HomeContainer />} />
->>>>>>> b924f24936d17ecb316e8cb828319f7e6d8df567
->>>>>>> Azul-Back
         <Route path="/firstFilters" element={<FirstFilters />} />
-        <Route path="/secondFilters" element={<SecondFilters />} />
         <Route path="/logIN" element={<LogIn />} />
         <Route path="/about" element={<About />} />
       </Routes>
