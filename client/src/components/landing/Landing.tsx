e2a6143178586be8dc6e712ea616c074967c8041
Carlunchi <109547028+Carlunchi@users.noreply.github.com>
2023-05-03 19:15:09 -0300

    solucion errores Login/register y form venta/tasar

--- client/src/components/landing/Landing.tsx
+++ client/src/components/landing/Landing.tsx
@@ -7,7 +7,7 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-import logo from "../../image/logo.png";
+import logo from '../../image/logo.png';
 
 export const Landing = () => {
   const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
