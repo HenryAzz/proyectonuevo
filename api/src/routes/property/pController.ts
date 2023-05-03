48a30131cfa966c6b319146857ba93da8668f08f
Azul <azschiaffino@gmail.com>
2023-05-03 20:05:21 -0300

    README explicativo de rutas Back. Property Actualizada

--- api/src/routes/property/pController.ts
+++ api/src/routes/property/pController.ts
@@ -1,37 +1,43 @@
-
 import { Response, Request } from "express";
-import { findProps } from "./pHelper";
+import { findProps, deleteP } from "./pHelper";
 import { sequelize } from "../../db";
 
-
 //Traemos la tabla de nuestra DB.
-const {Property} = sequelize.models
+const { Property } = sequelize.models;
 
 //  GET PROPERTY  //
-export const getProp= async (req: Request, res: Response) => {
-  //Tratamos errores por buenas practicas.   
+export const getProp = async (req: Request, res: Response) => {
+  //Tratamos errores por buenas practicas.
   try {
-   const propertys = await findProps() //helper trae todas las props.
+    const propertys = await findProps(); //helper trae todas las props.
     return res.status(200).json(propertys);
-      
-    } catch (error) {
-      return res.status(404).send({"error":error}) //enviar tipo de error
-    }
+  } catch (error) {
+    return res.status(404).send({ error: error }); //enviar tipo de error
   }
+};
 ///////////////////
-
-
 
 // POST PROPERTY //
 
 //(Request y Response representan el Tipo de objeto de Req y Res. No los llamamos por su nombre completo en ningun momento)
-  export const postProp= async (req: Request, res: Response) => {
-    try {
-      const db = await Property.create(req.body);
-      res.send({"msj": "Creado correctamente"});
-    } catch (error) {
-      res.status(404).send(error);  
-    }
+export const postProp = async (req: Request, res: Response) => {
+  try {
+    const db = await Property.create(req.body);
+    res.send({ msj: "Creado correctamente" });
+  } catch (error) {
+    return res.status(404).send({ error: error });
   }
- 
-  /////////////////////
+};
+
+/////////////////////Delete Prop
+
+export const deleteProp = async (req: Request, res: Response) => {
+  let { id } = req.params;
+  const idNum: number = Number(id);
+  try {
+    const property = await deleteP(idNum);
+    res.status(200).json(property);
+  } catch (error: any) {
+    return res.status(404).send({ error: error });
+  }
+};
--- client/src/components/logIn/LogIn.tsx
+++ client/src/components/logIn/LogIn.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Grid,
   Paper,
@@ -6,16 +5,15 @@ import {
   TextField,
   Button,
   Typography,
-  Link,
   Checkbox,
   FormControlLabel,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-
+import { Link } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-export const LogIn = ( handleChange ) => {
+export const LogIn = (handleChange: any) => {
   const paperStyle = {
     padding: 20,
     height: "73vh",
@@ -30,12 +28,12 @@ export const LogIn = ( handleChange ) => {
     remember: false,
   };
   const validationSchema = Yup.object().shape({
-    username: Yup
-    .string().required("Required")
-    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, "Enter valid email"),
+    username: Yup.string()
+      .required("Required")
+      .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, "Enter valid email"),
     password: Yup.string().required("Password required"),
   });
-  const onSubmit = (values, props) => {
+  const onSubmit = (values: any, props: any) => {
     console.log(values);
     setTimeout(() => {
       props.resetForm();
@@ -43,13 +41,15 @@ export const LogIn = ( handleChange ) => {
     }, 2000);
   };
   return (
-    <Grid   container 
-            direction='column-reverse' 
-            alignItems='center' 
-            justifyContent='center'
-            sx={{ minHeight:'100vh'}}>
+    <Grid
+      container
+      direction="column-reverse"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh" }}
+    >
       <Paper style={paperStyle}>
-        <Grid >
+        <Grid>
           <Avatar style={avatarStyle}>
             <LockOutlinedIcon />
           </Avatar>
@@ -87,26 +87,26 @@ export const LogIn = ( handleChange ) => {
                 control={<Checkbox color="primary" />}
                 label="Remember me"
               />
-              <Button
-                type="submit"
-                color="primary"
-                variant="contained"
-                disabled={props.isSubmitting}
-                style={btnstyle}
-                fullWidth
-              >
-                {props.isSubmitting ? "Loading" : "Sign in"}
-              </Button>
+              <Link to="/home">
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  disabled={props.isSubmitting}
+                  style={btnstyle}
+                  fullWidth
+                >
+                  {props.isSubmitting ? "Loading" : "Sign in"}
+                </Button>
+              </Link>
             </Form>
           )}
         </Formik>
-        <Typography>
-          <Link href="#">Forgot password ?</Link>
-        </Typography>
+        <Typography>Forgot password</Typography>
         <Typography>
           {" "}
           Do you have an account ?
-          <Link href="/formularioRegistro" onClick={() => handleChange("event", 1)}>
+          <Link to="/formularioRegistro" onClick={() => handleChange("event", 1)}>
             Sign Up
           </Link>
         </Typography>
@@ -114,118 +114,3 @@ export const LogIn = ( handleChange ) => {
     </Grid>
   );
 };
-
-
-
-// import  React, { FormEvent } from "react";
-// import { Button, Container, Grid, Paper, Box, Typography, TextField } from "@mui/material";
-// import { Link } from "react-router-dom";
-// import LockIcon from '@mui/icons-material/Lock';
-// import  InputAdornment from '@mui/material/InputAdornment'
-// import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircle';
-// // import logo from '../../image/'
-// import styled from "@emotion/styled";
-
-// type LoginType={
-//   username:string;
-//   password:string; 
-// }
-
-// export const LogIn = () => {
-
-//   const [loginData, setLoginData] = React.useState<LoginType>({
-//     username:'',
-//     password:'',
-// })
-
-// const dataLogin =(e: React.ChangeEvent<HTMLInputElement>) => {
-//   setLoginData({...loginData, [e.target.name]: e.target.value})
-// }
-
-// const handleSubmit = (e: FormEvent<HTMLInputElement>) =>{
-//   e.preventDefault();
-// }
-
-
-// const Img = styled("img")({
-//   width: 250,
-//   height: 50,
-//   display: 'flex',
-  
-// });
-
-//   return (
-//      <Container>
-//      {/* <Img
-//      src={logo}
-//      alt='logo'/> */}
-//       <Container 
-//       maxWidth='md'
-//       >
-         
-//         <Grid 
-//         container 
-//         direction='column-reverse' 
-//         alignItems='center' 
-//         justifyContent='center'
-//         sx={{ minHeight:'100vh'}}
-        
-//         >
-//           <Grid item>
-//           <Paper sx={{padding:'1.2em', borderRadius:'0.5em', backgroundColor:'#ffe0b2' }}>
-//             <Typography sx={{mt:1, mb:1}}variant='h4'>Welcome</Typography>
-//             <Box component='form' onSubmit={handleSubmit}>
-//                <TextField
-               
-//                InputProps={{
-//                 startAdornment: (
-//                   <InputAdornment position="start">
-//                      <AccountCircleRoundedIcon/>
-//                   </InputAdornment>)}}
-//                id="input-with-sx"
-//                variant="outlined"
-//                name='username'
-//                type='email'
-//                 margin='normal'
-//                 fullWidth
-//                 label='Email' 
-//                 sx={{mt:2, mb:1.5}} 
-//                 required
-//                 onChange={dataLogin}/>
-                
-//                <TextField 
-//                 InputProps={{
-//                 startAdornment: (
-//                   <InputAdornment position="start">
-//                     <LockIcon />
-//                   </InputAdornment>)}}
-//                id="input-with-sx"
-//                variant="outlined"
-//                name='password'
-//                type='password'
-//                margin='normal'
-//                fullWidth 
-//                label='Password' 
-//                sx={{mt:1.5, mb:1.5}} 
-//                required
-//                onChange={dataLogin}
-             
-//               />
-              
-//                <Link to="/formulario">
-//                <Button fullWidth
-//                 type='submit' variant='contained' sx={{mt:1.5, mb:3}}>Sing in</Button>
-//                 </Link>
-//                 <Link to="/formularioRegistro">
-//                <Button fullWidth 
-//                 type='submit' variant='contained' sx={{mt:1, mb:1, bgcolor:"#0d47a1"}}>Register</Button>
-//                 </Link>
-//             </Box>
-//           </Paper>
-//           </Grid>
-//         </Grid>
-
-//       </Container>
-//       </Container>
-//   )
-// };
