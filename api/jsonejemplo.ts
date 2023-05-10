139b5d38413a9b64af17f7216c5de0df76a9a577
Azul <azschiaffino@gmail.com>
2023-05-10 15:01:15 -0300

    CRUD DE PROPERTY Y CRUD DE FORM LISTO. TABLA FORM CON RELACIONES LISTO, (ruta user y broker con una propiedad agregada.)

--- api/jsonejemplo.ts
+++ api/jsonejemplo.ts
@@ -1,107 +1,498 @@
-export const json = [{
-  "type": "Vivienda",
-  "address": "UJUY AV. al 1700 103",
-  "spaces": 2,
-  "price": 184.0,
-  "pictures":[ 
-      {"img":"https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg"}]
-  ,
-  "floors": 10,
-  "covered_area": 37,
-  "bathroom": 1.5,
-  "bedroom": 1,
-  "furnished": true,
-  "description": "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-  "situation": "Disponible",
-  "total_area": 40,
-  "antiquity": 1,
-  "operation": "Alquiler",
-  "owner": "Schiaffino"
-},
-{
-  "type": "Vivienda",
-  "address": "RIO NEGRO NO SE CUANTO",
-  "spaces": 2,
-  "price": 94.0,
-  "pictures":[ 
-      {"img":"https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg"}]
-  ,
-  "floors": 10,
-  "covered_area": 37,
-  "bathroom": 1.5,
-  "bedroom": 1,
-  "furnished": true,
-  "description": "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-  "situation": "Disponible",
-  
-  "total_area": 40,
-  "antiquity": 1,
-  "operation": "Alquiler",
-  "owner": "Schiaffino"
-},
-{
-  "type": "Vivienda",
-  "address": "CONGRESO Y NO SE CUAL MAS",
-  "spaces": 2,
-  "price": 82.0,
-  "pictures":[ 
-      {"img":"https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg"}]
-  ,
-  "floors": 10,
-  "covered_area": 37,
-  "bathroom": 1.5,
-  "bedroom": 1,
-  "furnished": true,
-  "description": "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-  "situation": "Disponible",
-  
-  "total_area": 40,
-  "antiquity": 1,
-  "operation": "Venta",
-  "owner": "Schiaffino"
-},
-{
-  "type": "Local",
-  "address": "NUEVA YORK 1230",
-  "spaces": 2,
-  "price": 33.0,
-  "pictures":[ 
-      {"img":"https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg"}]
-  ,
-  "floors": 10,
-  "covered_area": 37,
-  "bathroom": 1.5,
-  "bedroom": 1,
-  "furnished": true,
-  "description": "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-  "situation": "Disponible",
-  
-  "total_area": 40,
-  "antiquity": 1,
-  "operation": "Alquiler",
-  "owner": "Schiaffino"
-},
-{
-  "type": "Local",
-  "address": "SENILLOSA ALGUN NUMERO",
-  "spaces": 2,
-  "price": 76.0,
-  "pictures":[ 
-      {"img":"https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg"}]
-  ,
-  "floors": 10,
-  "covered_area": 37,
-  "bathroom": 1.5,
-  "bedroom": 1,
-  "furnished": true,
-  "description": "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-  "situation": "Disponible",
-  
-  "total_area": 40,
-  "antiquity": 1,
-  "operation": "Venta",
-  "owner": "Schiaffino"
-}]
+export const json = [
+  {
+    type: "Vivienda",
+    address: "UJUY AV. al 1700 103",
+    spaces: 2,
+    price: 184.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zona: "Belgrano",
+  },
+  {
+    type: "Oficina",
+    address: "RIO NEGRO NO SE CUANTO",
+    spaces: 2,
+    price: 94.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
 
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zone: "Palermo Soho",
+  },
+  {
+    type: "Local",
+    address: "CONGRESO Y NO SE CUAL MAS",
+    spaces: 2,
+    price: 82.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
 
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Puerto Madero",
+  },
+  {
+    type: "Industria",
+    address: "NUEVA YORK 1230",
+    spaces: 2,
+    price: 33.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
 
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Palermo",
+  },
+  {
+    type: "Local",
+    address: "SENILLOSA ALGUN NUMERO",
+    spaces: 2,
+    price: 76.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Recoleta",
+  },
+  {
+    type: "Vivienda",
+    address: "UJUY AV. al 1700 103",
+    spaces: 2,
+    price: 184.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zona: "Belgrano",
+  },
+  {
+    type: "Oficina",
+    address: "RIO NEGRO NO SE CUANTO",
+    spaces: 2,
+    price: 94.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zone: "Palermo Soho",
+  },
+  {
+    type: "Local",
+    address: "CONGRESO Y NO SE CUAL MAS",
+    spaces: 2,
+    price: 82.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Puerto Madero",
+  },
+  {
+    type: "Industria",
+    address: "NUEVA YORK 1230",
+    spaces: 2,
+    price: 33.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Palermo",
+  },
+  {
+    type: "Local",
+    address: "SENILLOSA ALGUN NUMERO",
+    spaces: 2,
+    price: 76.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Recoleta",
+  },
+  {
+    type: "Vivienda",
+    address: "UJUY AV. al 1700 103",
+    spaces: 2,
+    price: 184.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zona: "Belgrano",
+  },
+  {
+    type: "Oficina",
+    address: "RIO NEGRO NO SE CUANTO",
+    spaces: 2,
+    price: 94.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zone: "Palermo Soho",
+  },
+  {
+    type: "Local",
+    address: "CONGRESO Y NO SE CUAL MAS",
+    spaces: 2,
+    price: 82.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Puerto Madero",
+  },
+  {
+    type: "Industria",
+    address: "NUEVA YORK 1230",
+    spaces: 2,
+    price: 33.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Palermo",
+  },
+  {
+    type: "Local",
+    address: "SENILLOSA ALGUN NUMERO",
+    spaces: 2,
+    price: 76.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Recoleta",
+  },
+  {
+    type: "Vivienda",
+    address: "UJUY AV. al 1700 103",
+    spaces: 2,
+    price: 184.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zona: "Belgrano",
+  },
+  {
+    type: "Oficina",
+    address: "RIO NEGRO NO SE CUANTO",
+    spaces: 2,
+    price: 94.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Alquiler",
+    owner: "Schiaffino",
+    zone: "Palermo Soho",
+  },
+  {
+    type: "Local",
+    address: "CONGRESO Y NO SE CUAL MAS",
+    spaces: 2,
+    price: 82.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Puerto Madero",
+  },
+  {
+    type: "Industria",
+    address: "NUEVA YORK 1230",
+    spaces: 2,
+    price: 33.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Palermo",
+  },
+  {
+    type: "Local",
+    address: "SENILLOSA ALGUN NUMERO",
+    spaces: 2,
+    price: 76.0,
+    pictures: [
+      {
+        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+      },
+    ],
+    floors: 10,
+    covered_area: 37,
+    bathroom: 1.5,
+    bedroom: 1,
+    furnished: true,
+    description:
+      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+    situation: "Disponible",
+
+    total_area: 40,
+    antiquity: 1,
+    operation: "Venta",
+    owner: "Schiaffino",
+    zone: "Recoleta",
+  },
+];
