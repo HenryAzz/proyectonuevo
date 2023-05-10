626f9a95283750f5c59e8532db3e1797ed98ce69
Gustavo Guaimas <gguaimas85@gmail.com>
2023-05-10 13:01:30 -0300

    Creacion Componente Detail

--- client/src/components/cardDetail/CardDetail.tsx
+++ client/src/components/cardDetail/CardDetail.tsx
@@ -1,15 +1,38 @@
+import { Box, Container, Typography, Grid } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useGetPropertyByIdQuery } from "../../reduxToolkit/apiSlice";
+import Divider from "@mui/material/Divider";
 
 export const CardDetail = () => {
   const { id } = useParams();
-
-  const idProperty: number = parseInt(id);
-  console.log(typeof idProperty);
-
-  const { data, isLoading } = useGetPropertyByIdQuery(idProperty);
+  const { data, isLoading } = useGetPropertyByIdQuery(id);
 
   console.log(data);
 
-  return <>Componente de detalles</>;
+  return (
+    <Box sx={{ width: "100%", mt: 10 }}>
+      <Container maxWidth="xl">
+        {isLoading ? (
+          <Typography variant="h4">Cargando...</Typography>
+        ) : (
+          <Grid
+            sx={{ mt: 2, display: "flex", justifyContent: "center" }}
+            container
+            direction="column"
+          >
+            <Grid item>
+              <img
+                src={data!.pictures[0].img}
+                style={{ width: "60vw", height: "70vh", borderRadius: "0.5em" }}
+              />
+            </Grid>
+            <Divider />
+            <Grid item>
+              <Typography variant="h3">{data!.type}</Typography>
+            </Grid>
+          </Grid>
+        )}
+      </Container>
+    </Box>
+  );
 };
