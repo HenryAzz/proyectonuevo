f7236c004c198f6295efada7bd1c404a4c068ca4
Azul <azschiaffino@gmail.com>
2023-05-15 12:26:18 -0300

    Merge ok

--- api/jsonejemplo.ts
+++ api/jsonejemplo.ts
@@ -1,120 +1,28 @@
 export const json = [
   {
-    type: "Vivienda",
-    address: "UJUY AV. al 1700 103",
-    spaces: 2,
-    price: 184.0,
+    //BARRIO NORTE
+    //"Vivienda", "Oficina", "Local", "Industria")
+    type: "Industria",
+    address: "Talcahuano al 800",
+    spaces: 39,
+    price: 197.0,
     pictures: [
       {
-        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
+        img: "https://static.tokkobroker.com/water_pics/85254660142641264679107666028497718208540059410689041719885236710420663391424.jpg",
       },
     ],
-    floors: 10,
-    covered_area: 37,
-    bathroom: 1.5,
-    bedroom: 1,
+    floors: 5,
+    covered_area: 1.0,
+    bathroom: 0,
+    bedroom: 39,
     furnished: true,
     description:
-      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
+      "Edificio comercial, ubicado sobre calle Talcahuano 844, entre av. Cordoba y Paraguay, Barrio Norte, CABA. Desarrollado en planta baja y cuatro pisos, comunicados por escaleras y ascensor. Cuenta con 30 habitaciones, todas con baño propio y en excelente estado de mantenimiento. SE PIDE MUEBLES Y UTILES/ ( U$ 180.000)...SE ESCUCHA PROPUESTA!! Planta baja: 210 m2 , 1ro a 4to piso: 200 m2 c/u . Sup. TOTAL: 1000 m2",
+    // "Reservado", "Disponible", "Rentado", "Vendido"
     situation: "Disponible",
-    total_area: 40,
-    antiquity: 1,
-    operation: "Alquiler",
-    owner: "Schiaffino",
-  },
-  {
-    type: "Vivienda",
-    address: "RIO NEGRO NO SE CUANTO",
-    spaces: 2,
-    price: 94.0,
-    pictures: [
-      {
-        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
-      },
-    ],
-    floors: 10,
-    covered_area: 37,
-    bathroom: 1.5,
-    bedroom: 1,
-    furnished: true,
-    description:
-      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-    situation: "Disponible",
-
-    total_area: 40,
-    antiquity: 1,
-    operation: "Alquiler",
-    owner: "Schiaffino",
-  },
-  {
-    type: "Vivienda",
-    address: "CONGRESO Y NO SE CUAL MAS",
-    spaces: 2,
-    price: 82.0,
-    pictures: [
-      {
-        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
-      },
-    ],
-    floors: 10,
-    covered_area: 37,
-    bathroom: 1.5,
-    bedroom: 1,
-    furnished: true,
-    description:
-      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-    situation: "Disponible",
-
-    total_area: 40,
-    antiquity: 1,
-    operation: "Venta",
-    owner: "Schiaffino",
-  },
-  {
-    type: "Local",
-    address: "NUEVA YORK 1230",
-    spaces: 2,
-    price: 33.0,
-    pictures: [
-      {
-        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
-      },
-    ],
-    floors: 10,
-    covered_area: 37,
-    bathroom: 1.5,
-    bedroom: 1,
-    furnished: true,
-    description:
-      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-    situation: "Disponible",
-
-    total_area: 40,
-    antiquity: 1,
-    operation: "Alquiler",
-    owner: "Schiaffino",
-  },
-  {
-    type: "Local",
-    address: "SENILLOSA ALGUN NUMERO",
-    spaces: 2,
-    price: 76.0,
-    pictures: [
-      {
-        img: "https://static.tokkobroker.com/water_pics/61473490849556420478476952427662215243829091586321796539503166833583826460719.jpg",
-      },
-    ],
-    floors: 10,
-    covered_area: 37,
-    bathroom: 1.5,
-    bedroom: 1,
-    furnished: true,
-    description:
-      "DPTO. 2 AMBIENTES, CFTE., BALCON,  APTO PROFESIONAL, AMENITIES, A ESTRENAR, ENTREGA INMEDIATA!",
-    situation: "Disponible",
-
-    total_area: 40,
-    antiquity: 1,
+    total_area: 1.0,
+    antiquity: 30,
+    // ("Alquiler", "Venta")
     operation: "Venta",
     owner: "Schiaffino",
   },
