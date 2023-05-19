94cabc9ef63b9ababad575e9ad9d7259fcd2b9ed
Carlunchi <109547028+Carlunchi@users.noreply.github.com>
2023-05-18 23:44:59 -0300

    Landing terminada

--- client/src/components/landing/Landing.tsx
+++ client/src/components/landing/Landing.tsx
@@ -1,48 +1,52 @@
-import { Container, Grid, Box } from "@mui/material";
-import video from "../../image/Diseño sin título_1.mp4";
+import { Container, Grid, Box, Button } from "@mui/material";
+import video from "../../image/video19.mp4";
 import { Link } from "react-router-dom";
 import s from "../landing/Landing.module.css";
 
 export const Landing = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Container>
-        <Grid container>
-          <Grid item xs={8} sm={8} md={6} lg={4}>
-            <video
-              id="video"
-              autoPlay
-              loop
-              muted
-              src={video}
-              style={{
-                position: "fixed",
-                width: "100%",
-                left: "50%",
-                top: "50%",
+    <Container>
+      <br />
+      <Grid container justifyContent="end" alignContent="end" position="fixed">
+        <Link to="/home">
+          <Button style={{ fontSize: 17 }} sx={{ color: "black" }} className={s.bt}>
+            <span>
+              <strong>INGRESAR</strong>
+            </span>
+          </Button>
+        </Link>
 
-                objectFit: "cover",
-                transform: "translate(-50%, -50%)",
-                zIndex: "-1",
-              }}
-            ></video>
-            <br />
-            <Box display="flex" justifyContent="center" alignContent="center" sx={{ ml: 80 }}>
-              <Link to="/home">
-                <strong>
-                  <button className={s.bt}>INGRESAR</button>
-                </strong>
-              </Link>
+        <Grid item xs={8} sm={8} md={6} lg={4}>
+          <video
+            id="video"
+            autoPlay
+            loop
+            muted
+            src={video}
+            style={{
+              position: "fixed",
+              width: "100%",
+              left: "50%",
+              top: "50%",
 
-              <Link to="/about">
-                <strong>
-                  <button className={s.bt}>NOSOTROS</button>
-                </strong>
-              </Link>
-            </Box>
-          </Grid>
+              objectFit: "cover",
+              transform: "translate(-50%, -50%)",
+              zIndex: "-1",
+            }}
+          ></video>
+          <br />
+          <div className={s.box}>
+            <div className={s.box2}>
+              CONTACTO:
+              <br />
+              Av. 9 de Julio 580
+              <br />
+              Whatsapp: 011-155895324 <br />
+              PF.prop.tech@gmail.com <br />
+            </div>
+          </div>
         </Grid>
-      </Container>
-    </div>
+      </Grid>
+    </Container>
   );
 };
