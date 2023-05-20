a861a2aec63e9dc21cf3ec89014995ded779d512
Carlunchi <109547028+Carlunchi@users.noreply.github.com>
2023-05-20 12:41:58 -0300

    Cambios About

--- client/src/components/about/About.tsx
+++ client/src/components/about/About.tsx
@@ -2,38 +2,92 @@ import logo from "../../image/logo.png";
 import { Typography, Grid, Avatar, Container, IconButton, Box, Button } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Link } from "react-router-dom";
-import { Carrousel } from "../carrousel/carrousel";
-import { imageVision, imageProposito, imageMision } from "../../image/imagesPath";
+
+import s from "./About.module.css";
 import { teamMembers } from "../../assets/imageTeam";
+import prop from "../../image/Prop.png";
+import mi from "../../image/Min.png";
+import vi from "../../image/Vi.png";
 
 export const About = () => {
-  const images = [imageMision, imageVision, imageProposito];
-
   return (
     <Box>
       <img src={logo} alt="logo" width="250px" height="50px" />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          mt: 12,
-          p: 1,
-          height: "79.5vh",
-        }}
-      >
-        <Box sx={{ maxWidth: 1000, flexGrow: 2 }}>
-          <Carrousel images={images} duration={15} />
-        </Box>
-      </Box>
-      <section style={{ height: "100vh" }}>
+      <section style={{ height: "90vh" }}>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={6} lg={4}>
+            <Box
+              sx={{
+                component: "grid",
+                margin: "50px",
+              }}
+            >
+              <div className={s.conteiner}>
+                <section className={s.section}>
+                  <div className={s.slide}>
+                    <div className={s.img}>
+                      <img src={mi} />
+                    </div>
+                    <div className={s.content}>
+                      <h2>Misión</h2>
+                      <div className={s.p}>
+                        <p>
+                          Nuestra plataforma propTech busca optimizar la experiencia de los clientes
+                          y brokers en el mercado inmobiliario de Buenos Aires, mediante la
+                          autoimatizació de tareas recurrentes, la agilización de la comunicación y
+                          la resolución eficiente de operaciones.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={s.slide}>
+                    <div className={s.img}>
+                      <img src={vi} />
+                    </div>
+                    <div className={s.content}>
+                      <h2>Visión</h2>
+                      <p>
+                        Nos esforzamos por consolidarnos como la plataforma líder en propiedades de
+                        lujo en zonas exclusivas de Buenos Aires, gracias a nuestra atención al
+                        cliente excepcional y nuestra capacidad de ofrecer soluciones personalizadas
+                        y de calidad para brokers y clientes{" "}
+                      </p>
+                    </div>
+                  </div>
+                  <div className={s.slide}>
+                    <div className={s.img}>
+                      <img src={prop} />
+                    </div>
+
+                    <div className={s.content}>
+                      <h2>Propósito</h2>
+                      <p>
+                        En nuestro propósito de ofrecer soluciuones eficientes y personalizadas,
+                        creemos en la importancia de una comunicación transparente y eficaz entre
+                        brokers y clientes. Por ello, nos enfocamos en generar confianza y
+                        satisfacción en la plataforma a través de una atención al cliente
+                        excepcional y una gestión óptima de las operaciones inmobiliarias
+                      </p>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </Box>
+          </Grid>
+        </Grid>
+      </section>
+      <section style={{ height: "90vh" }}>
         <Container>
           <Box my={8}>
             <Typography variant="h4" component="h1" align="center" fontWeight="600">
               Sobre Nosotros
             </Typography>
           </Box>
-          <Grid container spacing={4} sx={{ ml: "25px" }}>
+          <Grid
+            container
+            spacing={4}
+            sx={{ ml: "25px", background: "#f2c6845d", borderRadius: "20px" }}
+          >
             {teamMembers.map((member) => (
               <Grid item xs={12} sm={6} md={3} key={member.name}>
                 <Avatar
@@ -47,16 +101,12 @@ export const About = () => {
                 <Typography variant="subtitle2" color="black" fontWeight="bold" fontStyle="italic">
                   {member.role}
                 </Typography>
-                <Box sx={{ ml: 4 }}>
-                  <IconButton>
-                    <Link to={member.path}>
-                      <GitHubIcon sx={{ color: "#171515" }} />
-                    </Link>
+                <Box sx={{ ml: 4, mb: 5 }}>
+                  <IconButton component="a" href={member.path} target="_blank">
+                    <GitHubIcon sx={{ color: "#171515" }} />
                   </IconButton>
-                  <IconButton>
-                    <Link to={member.path2}>
-                      <LinkedInIcon sx={{ color: "#0e76a8" }} />
-                    </Link>
+                  <IconButton component="a" href={member.path2} target="_blank">
+                    <LinkedInIcon sx={{ color: "#0e76a8" }} />
                   </IconButton>
                 </Box>
               </Grid>
