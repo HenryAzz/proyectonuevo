da1b7157c3b948a91b6ea24a3861eef1ec0537e6
Azul <azschiaffino@gmail.com>
2023-05-26 11:01:41 -0300

    update broker

--- client/src/components/Admin/Detalle.ts
+++ client/src/components/Admin/Detalle.ts
@@ -1,15 +1,14 @@
+// const PropertyDetails = (selectedProperty) => {
+//   if (!selectedProperty) {
+//     return null; // No hay propiedad seleccionada, no se muestra nada
+//   }
 
-const PropertyDetails = (selectedProperty) => {
-  if (!selectedProperty) {
-    return null; // No hay propiedad seleccionada, no se muestra nada
-  }
-
-  return (
-    <div className="property-details">
-      {/* Renderiza la información detallada de la propiedad */}
-      <h2>{selectedProperty.address}</h2>
-      <p>{selectedProperty.description}</p>
-      {/* Otros campos de la propiedad */}
-    </div>
-  );
-};
+//   return (
+//     <div className="property-details">
+//       {/* Renderiza la información detallada de la propiedad */}
+//       <h2>{selectedProperty.address}</h2>
+//       <p>{selectedProperty.description}</p>
+//       {/* Otros campos de la propiedad */}
+//     </div>
+//   );
+// };
