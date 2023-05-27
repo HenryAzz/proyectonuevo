cf57b264399e6953e909eb2940ce4ff937c83512
Azul <azschiaffino@gmail.com>
2023-05-26 21:08:33 -0300

    nueva

--- client/src/components/review/Review.tsx
+++ client/src/components/review/Review.tsx
@@ -1,21 +1,39 @@
 import s from "./Review.module.css";
-import icon from "../../image/5066618.png";
-import icon2 from "../../image/5066622.png";
 import * as Yup from "yup";
-import { Box, Button, Paper, TextField } from "@mui/material";
-
-import logo from "../../image/logo.png";
-import { StarRating } from "./StarRating";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useState, useEffect } from "react";
-import { useCreateConsultMutation } from "../../reduxToolkit/apiSlice";
+import { useCreateConsultMutation, useGetReviewQuery } from "../../reduxToolkit/apiSlice";
 import Swal from "sweetalert2";
 import { auth } from "../../firebase/firebase";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 export const Review = () => {
+  const { data } = useGetReviewQuery();
   const [value, setValue] = useState("");
   const [user, setUser] = useState<string | null | undefined>(null);
-  const [createConsult, { data }] = useCreateConsultMutation();
+  const [
+    createConsult,
+    // { data }
+  ] = useCreateConsultMutation();
+
+  function convertirAEstrellas(numero: number) {
+    if (numero < 1 || numero > 5) {
+      return "Número fuera de rango";
+    }
+
+    const estrellas = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= numero) {
+        estrellas.push(<StarIcon sx={{ color: "yellow", fontSize: 35 }} key={i} />);
+      } else {
+        estrellas.push(<StarBorderIcon sx={{ fontSize: 35 }} key={i} />);
+      }
+    }
+
+    return estrellas;
+  }
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -32,6 +50,11 @@ export const Review = () => {
   const handleChange = (event: any) => {
     setValue(event.target.value);
   };
+
+  const testimonio = data?.map((testimonio: any) => {
+    return testimonio;
+  });
+  console.log(testimonio);
 
   const Toast = Swal.mixin({
     toast: true,
@@ -131,138 +154,41 @@ export const Review = () => {
           alignContent: "center",
           justifyContent: "center",
           padding: "20px",
-          gridTemplateColumns: "repeat(auto-fit, minmax(370px, 1fr))",
+          gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
         }}
       >
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
+        {testimonio?.map((element: any, index: any) => (
+          <Box
+            sx={{
+              my: 3,
+              mx: 2,
+
+              display: "grid",
+            }}
+            className={s.card}
+          >
+            <div className={s.layer}></div>
+            <div className={s.content}>
+              <div key={index}>
+                <Typography variant="h3" sx={{ fontSize: 30 }}>
+                  Broker: {element.target}
+                </Typography>
+
+                <strong>
+                  <h3> {convertirAEstrellas(element.grade)} </h3>
+                </strong>
+                <p>{element.message}</p>
+              </div>
+
+              {/* <div className={s.image}>
+                    <img src={icon} alt="avatar" />
+                  </div> */}
+              {/* <div className={s.details}>
+                  <h2>Sofía Francavilla</h2>
+                </div> */}
             </div>
-            <p>
-              Estoy muy conforme con la forma de atención de Samuel Bocanegra, es un chico que supo
-              comunicarme tranquilad al momento de mis inquietudes, como también al momento de la
-              seña de la casa. Recomendable al 100% esta empresa como a los vendedores.
-            </p>
-            <div className={s.image}>
-              <img src={icon} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Sofía Francavilla</h2>
-            </div>
-          </div>
-        </Box>
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
-            </div>
-            <p>
-              Estoy muy conforme con la forma de atención de Samuel Bocanegra, es un chico que supo
-              comunicarme tranquilad al momento de mis inquietudes, como también al momento de la
-              seña de la casa. Recomendable al 100% esta empresa como a los vendedores.
-            </p>
-            <div className={s.image}>
-              <img src={icon} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Sofía Francavilla</h2>
-            </div>
-          </div>
-        </Box>
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
-            </div>
-            <p>
-              Estoy muy conforme con la forma de atención de Samuel Bocanegra, es un chico que supo
-              comunicarme tranquilad al momento de mis inquietudes, como también al momento de la
-              seña de la casa. Recomendable al 100% esta empresa como a los vendedores.
-            </p>
-            <div className={s.image}>
-              <img src={icon} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Sofía Francavilla</h2>
-            </div>
-          </div>
-        </Box>
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
-            </div>
-            <p>
-              Estoy muy conforme con la forma de atención de Samuel Bocanegra, es un chico que supo
-              comunicarme tranquilad al momento de mis inquietudes, como también al momento de la
-              seña de la casa. Recomendable al 100% esta empresa como a los vendedores.
-            </p>
-            <div className={s.image}>
-              <img src={icon} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Sofía Francavilla</h2>
-            </div>
-          </div>
-        </Box>
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
-            </div>
-            <p>
-              Estoy muy conforme con la forma de atención de Samuel Bocanegra, es un chico que supo
-              comunicarme tranquilad al momento de mis inquietudes, como también al momento de la
-              seña de la casa. Recomendable al 100% esta empresa como a los vendedores.
-            </p>
-            <div className={s.image}>
-              <img src={icon} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Sofía Francavilla</h2>
-            </div>
-          </div>
-        </Box>
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
-            </div>
-            <p>
-              Estoy muy conforme con la forma de atención de Samuel Bocanegra, es un chico que supo
-              comunicarme tranquilad al momento de mis inquietudes, como también al momento de la
-              seña de la casa. Recomendable al 100% esta empresa como a los vendedores.
-            </p>
-            <div className={s.image}>
-              <img src={icon} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Sofía Francavilla</h2>
-            </div>
-          </div>
-        </Box>
-        <Box sx={{ my: 3, mx: 2 }} className={s.card}>
-          <div className={s.layer}></div>
-          <div className={s.content}>
-            <div>
-              <StarRating />
-            </div>
-            <p>me gusto como me atendio pepito</p>
-            <div className={s.image}>
-              <img src={icon2} alt="avatar" />
-            </div>
-            <div className={s.details}>
-              <h2>Santiago baraja </h2>
-            </div>
-          </div>
-        </Box>
+          </Box>
+        ))}
       </Box>
       <br />
       <Box
@@ -291,86 +217,85 @@ export const Review = () => {
           gridTemplateColumns: "repeat(auto-fit, minmax(370px, 1fr))",
         }}
       >
-        <Paper sx={{ background: "rgba(181, 179, 179, 0.384)" }}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-            sx={{ color: "black" }}
-          >
-            <Form>
-              <Box sx={{ display: "flex", gap: 2, my: 2 }}>
-                <Field
-                  as={TextField}
-                  fullWidth
-                  name="name"
-                  label="Nombre"
-                  placeholder="Nombre"
-                  inputProps={{ style: { color: "black" } }}
-                  InputLabelProps={{ style: { color: "black" } }}
-                  helperText={
-                    <strong>
-                      <ErrorMessage name="name" />
-                    </strong>
-                  }
-                />
-                <Field
-                  as={TextField}
-                  fullWidth
-                  name="email"
-                  label="Correo Electronico"
-                  placeholder="Correo Electronico"
-                  inputProps={{ style: { color: "black" } }}
-                  InputLabelProps={{ style: { color: "black" } }}
-                  helperText={
-                    <strong>
-                      <ErrorMessage name="email" />
-                    </strong>
-                  }
-                />
-              </Box>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+          sx={{ color: "black" }}
+        >
+          <Form>
+            <Box sx={{ display: "flex", gap: 2, my: 2 }}>
               <Field
                 as={TextField}
                 fullWidth
-                name="issue"
-                label="Asunto"
-                placeholder="Asunto de la consulta"
+                name="name"
+                label="Nombre"
+                placeholder="Nombre"
                 inputProps={{ style: { color: "black" } }}
                 InputLabelProps={{ style: { color: "black" } }}
                 helperText={
                   <strong>
-                    <ErrorMessage name="issue" />
+                    <ErrorMessage name="name" />
                   </strong>
                 }
               />
-              <Box>
-                <Field
-                  as={TextField}
-                  fullWidth
-                  multiline
-                  masRows={4}
-                  // value={value}
-                  // onChange={handleChange}
-                  name="description"
-                  label="Detallanos tu consulta"
-                  placeholder="Detallanos tu consulta"
-                  inputProps={{ style: { color: "black" } }}
-                  InputLabelProps={{ style: { color: "black" } }}
-                  helperText={
-                    <strong>
-                      <ErrorMessage name="description" />
-                    </strong>
-                  }
-                />
-              </Box>
-              <Box>
-                <Button type="submit" variant="contained">
-                  Enviar Consulta
-                </Button>
-              </Box>
-            </Form>
-          </Formik>
-        </Paper>
+              <Field
+                as={TextField}
+                fullWidth
+                name="email"
+                label="Correo Electronico"
+                placeholder="Correo Electronico"
+                inputProps={{ style: { color: "black" } }}
+                InputLabelProps={{ style: { color: "black" } }}
+                helperText={
+                  <strong>
+                    <ErrorMessage name="email" />
+                  </strong>
+                }
+              />
+            </Box>
+            <Field
+              as={TextField}
+              fullWidth
+              name="issue"
+              label="Asunto"
+              placeholder="Asunto de la consulta"
+              inputProps={{ style: { color: "black" } }}
+              InputLabelProps={{ style: { color: "black" } }}
+              sx={{ display: "flex", my: 2 }}
+              helperText={
+                <strong>
+                  <ErrorMessage name="issue" />
+                </strong>
+              }
+            />
+            <Box>
+              <Field
+                as={TextField}
+                fullWidth
+                multiline
+                masRows={4}
+                // value={value}
+                // onChange={handleChange}
+                name="description"
+                label="Detallanos tu consulta"
+                placeholder="Detallanos tu consulta"
+                inputProps={{ style: { color: "black" } }}
+                InputLabelProps={{ style: { color: "black" } }}
+                helperText={
+                  <strong>
+                    <ErrorMessage name="description" />
+                  </strong>
+                }
+              />
+            </Box>
+            <Box>
+              <Button type="submit" variant="contained">
+                Enviar Consulta
+              </Button>
+            </Box>
+          </Form>
+        </Formik>
       </Box>
     </Box>
   );
